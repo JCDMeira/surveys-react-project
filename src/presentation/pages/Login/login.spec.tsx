67e978b5c65e41aefdb0897e08377d27109182d3
Jean Meira <jean.meira10@hotmail.com>
2022-11-18 16:06:07 -0300

    refactor: move sut creation to a factory mrthod

--- src/presentation/pages/Login/login.spec.tsx
+++ src/presentation/pages/Login/login.spec.tsx
@@ -1,22 +1,31 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, RenderResult } from '@testing-library/react';
 import Login from './index';
+
+type SutTypes = {
+  sut: RenderResult;
+};
+
+const makeSut = (): SutTypes => {
+  const sut = render(<Login />);
+  return { sut };
+};
 
 describe('Login component', () => {
   test('should not render spinner and error on start', () => {
-    const { getByTestId } = render(<Login />);
+    const { sut } = makeSut();
 
-    const errorwrap = getByTestId('error-wrap');
+    const errorwrap = sut.getByTestId('error-wrap');
     expect(errorwrap.childElementCount).toBe(0);
 
-    const submitButton = getByTestId('submit') as HTMLButtonElement;
+    const submitButton = sut.getByTestId('submit') as HTMLButtonElement;
     expect(submitButton.disabled).toBe(true);
 
-    const emailStatus = getByTestId('email-status');
+    const emailStatus = sut.getByTestId('email-status');
     expect(emailStatus.title).toBe('Campo obrigatório');
     expect(emailStatus.textContent).toBe('🔴');
 
-    const passwordStatus = getByTestId('password-status');
+    const passwordStatus = sut.getByTestId('password-status');
     expect(passwordStatus.title).toBe('Campo obrigatório');
     expect(passwordStatus.textContent).toBe('🔴');
   });
