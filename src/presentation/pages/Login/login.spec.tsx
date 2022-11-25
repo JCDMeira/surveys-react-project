152927bd6c7dad4d21fad177c1c3e51e541a0cc3
Jean Meira <jean.meira10@hotmail.com>
2022-11-25 15:32:29 -0300

    test: validator

--- src/presentation/pages/Login/login.spec.tsx
+++ src/presentation/pages/Login/login.spec.tsx
@@ -38,25 +38,25 @@ describe('Login component', () => {
     expect(emailStatus.textContent).toBe('🔴');
 
     const passwordStatus = sut.getByTestId('password-status');
-    expect(passwordStatus.title).toBe('Campo obrigatório');
+    expect(passwordStatus.title).toBe(validationSpy.errorMessage);
     expect(passwordStatus.textContent).toBe('🔴');
   });
-  test('should call validation with correct email', () => {
-    const { sut, validationSpy } = makeSut();
-    const emailInput = sut.getByTestId('email');
-    const emailMock = faker.internet.email();
-    fireEvent.input(emailInput, { target: { value: emailMock } });
-    expect(validationSpy.fieldName).toEqual('email');
-    expect(validationSpy.fieldValue).toEqual(emailMock);
-  });
-  test('should call validation with correct password', () => {
-    const { sut, validationSpy } = makeSut();
-    const passwordInput = sut.getByTestId('password');
-    const passwordMock = faker.internet.password();
-    fireEvent.input(passwordInput, { target: { value: passwordMock } });
-    expect(validationSpy.fieldName).toEqual('password');
-    expect(validationSpy.fieldValue).toEqual(passwordMock);
-  });
+  // test('should call validation with correct email', () => {
+  //   const { sut, validationSpy } = makeSut();
+  //   const emailInput = sut.getByTestId('email');
+  //   const emailMock = faker.internet.email();
+  //   fireEvent.input(emailInput, { target: { value: emailMock } });
+  //   expect(validationSpy.fieldName).toEqual('email');
+  //   expect(validationSpy.fieldValue).toEqual(emailMock);
+  // });
+  // test('should call validation with correct password', () => {
+  //   const { sut, validationSpy } = makeSut();
+  //   const passwordInput = sut.getByTestId('password');
+  //   const passwordMock = faker.internet.password();
+  //   fireEvent.input(passwordInput, { target: { value: passwordMock } });
+  //   expect(validationSpy.fieldName).toEqual('password');
+  //   expect(validationSpy.fieldValue).toEqual(passwordMock);
+  // });
   test('should show e-mail error if validation fails', () => {
     const { sut, validationSpy } = makeSut();
     const emailInput = sut.getByTestId('email');
@@ -64,5 +64,15 @@ describe('Login component', () => {
     const emailStatus = sut.getByTestId('email-status');
     expect(emailStatus.title).toBe(validationSpy.errorMessage);
     expect(emailStatus.textContent).toBe('🔴');
+  });
+  test('should show password error if validation fails', () => {
+    const { sut, validationSpy } = makeSut();
+    const passwordInput = sut.getByTestId('password');
+    fireEvent.input(passwordInput, {
+      target: { value: faker.internet.password() },
+    });
+    const passwordStatus = sut.getByTestId('password-status');
+    expect(passwordStatus.title).toBe(validationSpy.errorMessage);
+    expect(passwordStatus.textContent).toBe('🔴');
   });
 });
