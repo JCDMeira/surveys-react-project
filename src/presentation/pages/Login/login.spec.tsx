9220bd5f26c26f2e6c3503a065014069c79c0bbe
Jean Meira <jean.meira10@hotmail.com>
2022-11-26 12:11:54 -0300

    test: ensure Login present valid sate if password is valid

--- src/presentation/pages/Login/login.spec.tsx
+++ src/presentation/pages/Login/login.spec.tsx
@@ -75,4 +75,15 @@ describe('Login component', () => {
     expect(passwordStatus.title).toBe(validationStub.errorMessage);
     expect(passwordStatus.textContent).toBe('🔴');
   });
+  test('should show valid password state if validation succeds', () => {
+    const { sut, validationStub } = makeSut();
+    validationStub.errorMessage = null;
+    const passwordInput = sut.getByTestId('password');
+    fireEvent.input(passwordInput, {
+      target: { value: faker.internet.password() },
+    });
+    const passwordStatus = sut.getByTestId('password-status');
+    expect(passwordStatus.title).toBe('Tudo certo');
+    expect(passwordStatus.textContent).toBe('🟢');
+  });
 });
