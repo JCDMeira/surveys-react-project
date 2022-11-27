24aab1ca8b9019f38badb111f0228a0a39c725fa
Jean Meira <jean.meira10@hotmail.com>
2022-11-27 16:40:58 -0300

    test: ensure Login present spinner on submit

--- src/presentation/pages/Login/login.spec.tsx
+++ src/presentation/pages/Login/login.spec.tsx
@@ -116,4 +116,22 @@ describe('Login component', () => {
     const submitButton = sut.getByTestId('submit') as HTMLButtonElement;
     expect(submitButton.disabled).toBe(false);
   });
+
+  test('should show spinner on submit', () => {
+    const { sut } = makeSut();
+    const passwordInput = sut.getByTestId('password');
+    fireEvent.input(passwordInput, {
+      target: { value: faker.internet.password() },
+    });
+    const emailInput = sut.getByTestId('email');
+    fireEvent.input(emailInput, {
+      target: { value: faker.internet.email() },
+    });
+
+    const submitButton = sut.getByTestId('submit');
+    fireEvent.click(submitButton);
+
+    const spinner = sut.getByTestId('spinner');
+    expect(spinner).toBeTruthy();
+  });
 });
